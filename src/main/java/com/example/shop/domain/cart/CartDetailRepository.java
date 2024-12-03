ab90c3e53dfd8d1cc0aa866c6af68f36d3cf725a
leeys9423 <leeys9423@gmail.com>
2024-12-03 20:13:55 +0900

    refactor: 장바구니에 기존 물품 여부 체크시 쿼리 최적화

--- src/main/java/com/example/shop/domain/cart/CartDetailRepository.java
+++ src/main/java/com/example/shop/domain/cart/CartDetailRepository.java
@@ -13,7 +13,6 @@ public interface CartDetailRepository extends JpaRepository<CartDetail, Long> {
     @Query("SELECT c FROM CartDetail c JOIN FETCH c.product WHERE c.user.id = :userId")
     List<CartDetail> findByUserIdWithProduct(@Param("userId") Long userId);
 
-    @Query("select c from CartDetail c join fetch c.product where c.user.id = :userId and c.product.id = :productId")
     Optional<CartDetail> findByUserIdAndProductId(@Param("userId") Long userId, @Param("productId") Long productId);
 
     void deleteByUserIdAndProductId(Long userId, Long productId);
