363417f548d801dbbbf2aaad3dfd58e8fec2e506
HeoJunSeo <wnstj56465@gmail.com>
2024-12-03 21:57:59 +0900

    feature/#40-물품수정 기능입니다

--- src/main/java/com/example/shop/admin/dao/AdminDAO.java
+++ src/main/java/com/example/shop/admin/dao/AdminDAO.java
@@ -3,14 +3,14 @@ package com.example.shop.admin.dao;
 
 import com.example.shop.admin.dto.ProductUpdateRequest;
 import com.example.shop.admin.mapper.AdminMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
+@RequiredArgsConstructor
 @Repository
 public class AdminDAO {
 
-    @Autowired
-    private AdminMapper adminMapper;
+    private final AdminMapper adminMapper;
 
     public int  updateProduct(ProductUpdateRequest productUpdateRequest) {
         return adminMapper.updateProduct(productUpdateRequest);
