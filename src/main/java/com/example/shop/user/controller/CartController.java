c7ee721dcd39ba0a45720f52b0430650bbf1b392
leeys9423 <leeys9423@gmail.com>
2024-12-03 16:19:40 +0900

    refactor: 장바구니 컨트롤러 사용하지 않은 import 삭제

--- src/main/java/com/example/shop/user/controller/CartController.java
+++ src/main/java/com/example/shop/user/controller/CartController.java
@@ -6,15 +6,9 @@ import com.example.shop.user.service.CartService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/cart")
