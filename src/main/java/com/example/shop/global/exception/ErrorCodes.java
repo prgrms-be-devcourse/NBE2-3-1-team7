2d561d1df31269e9cebd336c0a1c0c46d6bd2c3d
DongminL <milkskfk12@gmail.com>
2024-12-03 09:50:08 +0900

    feat: 이메일 전송 기능

--- src/main/java/com/example/shop/global/exception/ErrorCodes.java
+++ src/main/java/com/example/shop/global/exception/ErrorCodes.java
@@ -10,8 +10,11 @@ public enum ErrorCodes {
     NO_AUTH_ACCESS_TOKEN(HttpStatus.BAD_REQUEST,"액세스 토큰이 존재하지 않습니다"),
     NO_AUTHORIZATION_HEADER(HttpStatus.BAD_REQUEST,"Authorization이 존재하지 않습니다"),
     NO_BEARER(HttpStatus.BAD_REQUEST,"bearer 타입 토큰이 존재하지 않습니다"),
-    LOG_IN_NOT_MATCH(HttpStatus.BAD_REQUEST, "로그인 정보가 일치하지 않습니다");
+    LOG_IN_NOT_MATCH(HttpStatus.BAD_REQUEST, "로그인 정보가 일치하지 않습니다"),
 
+    // Email 관련 에러
+    EMAIL_FORM_CREATION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "이메일 템플릿 생성 중 오류가 발생했습니다."),
+    EMAIL_SEND_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "Email 서버에 문제가 발생하였습니다.");
 
     private String message;
     private HttpStatus status;
